460d8945b452c005f7e7b2b1791c098fda3456ef
Kumar Gupta <akshay.a.kumar.gupta@accenture.com>
2019-07-24 17:56:45 +0530

    Reactive Forms

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { FormControl , FormGroup , Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactiveform',
@@ -7,36 +8,40 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./reactiveform.component.scss']
 })
 export class ReactiveformComponent implements OnInit {
+formGroup = new FormGroup(
+  {
+  fullname :new FormControl('' , Validators.required),
+  lastname :new FormControl('' , Validators.compose([Validators.minLength(6), Validators.maxLength(16)])),
+  mobile :new FormControl(''),
+  password :new FormControl(''),
+  email :new FormControl(''),
+  address :new FormControl(''),
+    }
+  );
 
- formGroup = new FormGroup(
-   {
-     fname :new FormControl('',Validators.compose([Validators.minLength(4), Validators.maxLength(16), Validators.required])),
-     lname :new FormControl('',Validators.compose([Validators.minLength(4), Validators.maxLength(16)])),
-     mobile :new FormControl(''),
-     password :new FormControl(''),
-     email :new FormControl(''),
-     address :new FormControl(''),
-
-   }
- )
-
- onSubmit()
- {
-   console.log(this.formGroup);
- }
-
- resetForm()
- {
-   this.formGroup.reset();
- }
-
-  constructor() 
-  { 
-  
+  onSubmit()
+  {
+    console.log(this.formGroup.value);
   }
 
-
-  ngOnInit() {
+  resetForm()
+  {
+    this.formGroup.reset();
   }
 
-}
+  constructor() { }
+  ngOnInit() { }
+
+//  onSubmit()
+//  {
+//    console.log(this.formGroup);
+//  }
+
+//  resetForm()
+//  {
+//    this.formGroup.reset();
+//  }
+
+ 
+
+ }
